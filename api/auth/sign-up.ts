bffc1e5d3bb92e7583a4d70725b35aaddb825032
Yu Jin <qldls0307@naver.com>
2026-05-28 10:33:31 +0900

    fix: auth API 타입 import 분리

--- api/auth/sign-up.ts
+++ api/auth/sign-up.ts
@@ -1,4 +1,5 @@
-import { ApiRequest, ApiResponse, forwardAuthRequest } from "./_proxy";
+import { forwardAuthRequest } from "./_proxy";
+import type { ApiRequest, ApiResponse } from "./_proxy";
 
 export default async function handler(req: ApiRequest, res: ApiResponse) {
   await forwardAuthRequest(req, res, "/auth/sign-up");
